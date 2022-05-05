4136b62eb766cb950a81183dbf51dbb9f0d8ad46
TheVillainCoder <101288516+TheVillainCoder@users.noreply.github.com>
2022-05-05 19:54:41 -0400

    SPAM CRATES IS OUT OF BETA!

---
+++ scripts/spamCrates.js
@@ -0,0 +1,23 @@
+let name = prompt("Which crate would you like to open?\n\Options:\nall\nadd");
+let amt;
+let i = 1;
+amt = Number(prompt("How many crates would you like to open?"));
+
+function buyBox() {
+    var postData = 'crate=' + name;
+    $.post('/api/open/', postData, function(data) {
+        console.log('%c%s', 'color: white; font-size: 25px; text-shadow: 0px 0px 15px black;', `${data}`);
+        if (data === "You're being rate limited.") {
+            i--;
+        }
+    });
+}
+var check = setInterval(() => {
+    if (i <= amt) {
+        buyBox();
+        i++;
+    } else {
+        clearInterval(check);
+        alert("Done buying boxes! Check the console or your Elements to see what you got.");
+    }
+}, 1001);
