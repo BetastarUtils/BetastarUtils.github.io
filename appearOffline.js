73ee2da4796b6f3e347a297ce24449a55fe154bf
TheVillainCoder <101288516+TheVillainCoder@users.noreply.github.com>
2022-05-05 19:51:37 -0400

    Unintended side effects.

--- appearOffline.js
+++
@@ -1,4 +0,0 @@
-socket.disconnect()
-if (location.pathname === '/stats/' || location.pathname === '/stats') {
-  document.getElementById('#userElement').style.filter = 'drop-shadow(0px 0px 100px red)'
-}
