e8cfe2aecc5485c85d537b26d9bcd93735c20e56
VillainsRule <101288516+VillainsRule4000@users.noreply.github.com>
2022-05-15 18:28:52 -0400

    Version 4.1 - Betastar Update

--- scripts/getAtoms.js
+++ scripts/getAtoms.js
@@ -1,3 +1,4 @@
+alert('Script coded by Zastix, Betastar tester\nFor more scripts, visit\nhttps://villainsrule2000.github.io/Betastar/')
 alert('Started adding atoms! To stop, refresh the page.')
 
 function updateAtoms() {
