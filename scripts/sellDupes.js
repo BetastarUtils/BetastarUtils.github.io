5d6e378a82bb935ba512f47ee036079685b1c27e
VillainsRule <101288516+VillainsRule4000@users.noreply.github.com>
2022-05-15 18:29:18 -0400

    Version 4.1 - Betastar Update

--- scripts/sellDupes.js
+++ scripts/sellDupes.js
@@ -1,3 +1,5 @@
+alert('Script coded by Zastix, Betastar tester\nFor more scripts, visit\nhttps://villainsrule2000.github.io/Betastar/')
+
 async function sell(element) {
 	let amt = userElements[element] - 1
 	if (0 >= amt) return;
