29e290c80cff7528632fbaef848efae8d7992473
Hankypoo7 <100436822+Hankypoo7@users.noreply.github.com>
2022-05-19 20:30:33 -0500

    Create SellDupes.js

---
+++ Global Scripts/SellDupes.js
@@ -0,0 +1,13 @@
+//coded by zastix//
+async function sell(element) {
+	let amt = userElements[element] - 1
+	if (0 >= amt) return;
+	var postData = `element=${element}&quantity=${amt}`;
+	$.post(`/api/sell/`, postData, function() {
+		console.log(`Sold ${amt} ${element}(s)`)
+	})
+}
+
+Object.keys(elementList).forEach(element => {
+	sell(element)
+})
