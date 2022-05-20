384fef529f5485ceed3c2ac2bc5a12fdb2a60667
Hankypoo7 <100436822+Hankypoo7@users.noreply.github.com>
2022-05-19 20:23:15 -0500

    Create AppearOffline.js

---
+++ Utility/AppearOffline.js
@@ -0,0 +1,6 @@
+//coded by zastix//
+//note-you will not be able to trade with this hack enabled untill turned off//
+socket.disconnect()
+if (location.pathname === '/stats/' || location.pathname === '/stats') {
+  document.getElementById('#userElement').style.filter = 'drop-shadow(0px 0px 100px red)'
+}
