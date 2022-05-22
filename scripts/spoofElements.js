10022fbbd5ec23ac93070e7f1d7b54bbabb583e4
VillainsRule <101288516+VillainsRule4000@users.noreply.github.com>
2022-05-22 17:05:57 -0400

    Spoof Elements Update

--- scripts/spoofElements.js
+++ scripts/spoofElements.js
@@ -1,13 +1,12 @@
 alert('Script coded by Zastix, Betastar tester\nFor more scripts, visit\nhttps://villainsrule2000.github.io/Betastar/')
 
-for (i=0;i<document.getElementById('#elementList').children.length;i++) {
-    document.getElementById('#elementList').children[i].remove();
-}
+Array.from(document.getElementById('#elementList').children).forEach(a => a.remove())
 Object.entries(elementList).forEach((entry) => {
 	const [key, value] = entry;
 	$(`<img id="${key}" src="${elementList[key].imageURL}" onclick="viewElement('${key}')" class="bottomElement">`).appendTo(".elementList");
 })
 for (i=0;i<Object.keys(elementList).length;i++) {
     elemes = Object.keys(elementList)
-    userElements[elemes[i]] = Math.floor(Math.random() * 100);
+    if (elemes[i] === 'nigger') userElements[elemes[i]] = Math.floor(Math.random() * 4);
+    else userElements[elemes[i]] = Math.floor(elementList[elemes[i]]['chance'] / 2 + Math.round(Math.random() * 20));
 }
