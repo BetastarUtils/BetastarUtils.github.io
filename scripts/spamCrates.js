a77122986876319e9400a66633590545bb22a503
VillainsRule <101288516+VillainsRule4000@users.noreply.github.com>
2022-05-23 06:30:33 -0400

    Spam Crates Update

--- scripts/spamCrates.js
+++ scripts/spamCrates.js
@@ -2,6 +2,17 @@ alert('Script coded by Zastix, Betastar tester\nFor more scripts, visit\nhttps:/
 
 var i = 0;
 var boxes = []
+colors = {
+    divine: '#ee82ee',
+    mythical: '#a335ee',
+    perfect: '#fffacd',
+    fabled: '#0c7500',
+    legendary: '#ff910f',
+    epic: '#be0000',
+    rare: '#0a14fa',
+    uncommon: '#4bc22e',
+    common: '#ffffff'
+}
 Object.keys(cratesList).forEach(e => {
     boxes.push(e)
 })
@@ -10,16 +21,23 @@ if (!boxes.includes(name)) {
     alert('That crate doesn\'t exist...')
     name = prompt("Which crate would you like to open?\n\nOptions:\n" + boxes.join('\n'));
 }
-var amt = Number(prompt("How many crates would you like to open?"));
-
+var amt = Number(prompt("How many crates would you like to open?\ntype \"*\" to unlock all you can with your current atoms."));
+if (isNaN(amt)) amt = 99999999999999
 function buyBox() {
     $.post('/api/open/', `crate=${name}`, function(data) {
-        if (data === "You're being rate limited.") i--;
-        else console.log('%c%s', 'color: white; font-size: 25px; text-shadow: 0px 0px 15px black;', `${data}`);
+        try {
+            if (data === "You're being rate limited.") i--
+            else {
+                rarity = elementList[data]['rarity'].toLowerCase()
+                console.log('%c%s', `color: ${colors[rarity]}; font-size: 25px; text-shadow: 0px 0px 15px ${colors[rarity]};`, `${data}`);
+            }
+        } catch (e) {
+            i = amt
+        }
     });
 }
 var check = setInterval(() => {
-    if (i <= amt) {
+    if (i < amt) {
         buyBox();
         i++;
     } else {
