e15442b60cd131fe1ec956447c6001f8ba880640
VillainsRule <101288516+VillainsRule4000@users.noreply.github.com>
2022-06-13 06:33:38 -0400

    Add Elements Page

---
+++ info/elements/elements.js
@@ -0,0 +1,280 @@
+var elements = {
+    "Bismuth" : [
+      "Divine",
+      "25,000 atoms"
+    ],
+    "Platinum" : [
+      "Legendary",
+      "250 atoms"
+    ],
+    "Titanium" : [
+      "Legendary",
+      "200 atoms"
+    ],
+    "Osmium" : [
+      "Legendary",
+      "200 atoms"
+    ],
+    "Gold" : [
+      "Legendary",
+      "200 atoms"
+    ],
+    "Cobalt" : [
+      "Legendary",
+      "200 atoms"
+    ],
+    "Mercury" : [
+      "Legendary",
+      "200 atoms"
+    ],
+    "Niobium" : [
+      "Epic",
+      "80 atoms"
+    ],
+    "Iron" : [
+      "Epic",
+      "75 atoms"
+    ],
+    "Silver" : [
+      "Epic",
+      "75 atoms"
+    ],
+    "Lead" : [
+      "Epic",
+      "75 atoms"
+    ],
+    "Chromium" : [
+      "Epic",
+      "75 atoms"
+    ],
+    "Tungsten" : [
+      "Epic",
+      "75 atoms"
+    ],
+    "Cadmium" : [
+      "Rare",
+      "20 atoms"
+    ],
+    "Thallium" : [
+      "Rare",
+      "20 atoms"
+    ],
+    "Gallium" : [
+      "Rare",
+      "20 atoms"
+    ],
+    "Aluminium" : [
+      "Rare",
+      "20 atoms"
+    ],
+    "Vanadium" : [
+      "Rare",
+      "20 atoms"
+    ],
+    "Nickel" : [
+      "Rare",
+      "20 atoms"
+    ],
+    "Iridium" : [
+      "Rare",
+      "20 atoms"
+    ],
+    "Tantalum" : [
+      "Rare",
+      "20 atoms"
+    ],
+    "Copper" : [
+      "Rare",
+      "20 atoms"
+    ],
+    "Technetium" : [
+      "Rare",
+      "20 atoms"
+    ],
+    "Rhodium" : [
+      "Rare",
+      "20 atoms"
+    ],
+    "Ruthenium" : [
+      "Rare",
+      "20 atoms"
+    ],
+    "Scandium" : [
+      "Rare",
+      "20 atoms"
+    ],
+    "Zirconium" : [
+      "Rare",
+      "20 atoms"
+    ],
+    "Molybdenum" : [
+      "Uncommon",
+      "5 atoms"
+    ],
+    "Zinc" : [
+      "Uncommon",
+      "5 atoms"
+    ],
+    "Manganese" : [
+      "Uncommon",
+      "5 atoms"
+    ],
+    "Hafnium" : [
+      "Uncommon",
+      "5 atoms"
+    ],
+    "Yttrium" : [
+      "Uncommon",
+      "5 atoms"
+    ],
+    "Tin" : [
+      "Uncommon",
+      "5 atoms"
+    ],
+    "Rhenium" : [
+      "Uncommon",
+      "5 atoms"
+    ],
+    "Indium" : [
+      "Uncommon",
+      "5 atoms"
+    ],
+    "Polonium" : [
+      "Uncommon",
+      "5 atoms"
+    ],
+    "Palladium" : [
+      "Uncommon",
+      "5 atoms"
+    ],
+    "Uranium" : [
+      "Legendary",
+      "250 atoms"
+    ],
+    "Plutonium" : [
+      "Epic",
+      "75 atoms"
+    ],
+    "Neptunium" : [
+      "Rare",
+      "20 atoms",
+    ],
+    "Actinium" : [
+      "Rare",
+      "20 atoms",
+    ],
+    "Protactinium" : [
+      "Uncommon",
+      "5 atoms",
+    ],
+    "Thorium" : [
+      "Uncommon",
+      "5 atoms"
+    ],
+    "Yellow Xenon" : [
+      "Perfect",
+      "500 atoms"
+    ],
+    "Blue Krypton" : [
+      "Fabled",
+      "300 atoms"
+    ],
+    "Xenon" : [
+      "Legendary",
+      "250 atoms"
+    ],
+    "Krypton" : [
+      "Legendary",
+      "200 atoms"
+    ],
+    "Argon" : [
+      "Legendary",
+      "200 atoms"
+    ],
+    "Neon" : [
+      "Legendary",
+      "200 atoms"
+    ],
+    "Radon" : [
+      "Legendary",
+      "200 atoms"
+    ],
+    "Helium" : [
+      "Legendary",
+      "200 atoms"
+    ]
+}
+function viewElementBetastar() {
+document.getElementById("#elementRarity").innerText = "Common";
+document.getElementById("#elementRarity").style.color = "#FFFFFF";
+document.getElementById("#elementRarity").style.textShadow = "0px 0px 25px #FFFFFF";
+document.getElementById("#elementImage").src = "https://betastar.org/image/elements/blax.png";
+document.getElementById("#elementName").innerText = "Betastar";
+document.getElementById("#elementPrice").innerText = "0 atoms";
+}
+
+function viewElement(name) {
+    var elem = elements[name];
+    document.getElementById("#elementRarity").textContent = elem[0];
+    var color = elem[0];
+    switch (color) {
+        case "Common":
+            color = "FFFFFF";
+            break;
+        case "Uncommon":
+            color = "#4bc22e";
+            break;
+        case "Rare":
+            color = "#0a14fa";
+            break;
+        case "Epic":
+            color = "#be0000";
+            break;
+        case "Legendary":
+            color = "#ff910f";
+            break;
+        case "Fabled":
+            color = "#0c7500";
+            break;
+        case "Perfect":
+            color = "#fffacd";
+            break;
+    }
+    document.getElementById("#elementRarity").style.color = color;
+    document.getElementById("#elementRarity").style.textShadow = "0px 0px 25px " + color;
+    try {
+        if (typeof elem[2] != 'undefined') {
+                document.getElementById("#elementImage").src = "https://betastar.org/image/elements/"+elem[2]+".png";
+        } else {
+            document.getElementById("#elementImage").src = "https://betastar.org/image/elements/"+(name.charAt(0).toLowerCase() + name.slice(1)).replace(' ', '')+".png";
+        }
+    } catch (e) {
+        document.getElementById("#elementImage").src = "https://betastar.org/image/elements/"+(name.charAt(0).toLowerCase() + name.slice(1)).replace(' ', '')+".png";
+    }
+    document.getElementById("#elementName").textContent = name;
+    document.getElementById("#elementPrice").textContent = elem[1];
+}
+
+Object.entries(elements).forEach(function (item) {
+    var key = item[0].toString();
+    var elem = document.createElement('img');
+    
+    elem.classList.add('bottomElement');
+    
+    try {
+        if (typeof item[1][2] != 'undefined') {
+            elem.src = "https://betastar.org/image/elements/"+item[1][2]+".png";
+        } else {
+            elem.src = "https://betastar.org/image/elements/"+(key.charAt(0).toLowerCase() + key.slice(1)).replace(' ', '')+".png";
+        }
+    } catch (e) {
+        elem.src = "https://betastar.org/image/elements/"+(key.charAt(0).toLowerCase() + key.slice(1)).replace(' ', '')+".png";
+    }
+    
+    elem.onclick = function () {
+        viewElement(key);
+        window.currentQuantity = -1;
+    };
+    
+    document.getElementById('#elementList').appendChild(elem);
+});
